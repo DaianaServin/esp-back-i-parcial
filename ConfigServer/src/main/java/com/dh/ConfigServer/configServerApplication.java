4a65a5f2bef60163b45cd4e416ac6deec0445fe2
DaianaServin <daiana.servin@hotmail.com>
2022-11-14 00:03:21 -0300

    Microservicio gateway, config server, eureka server

---
+++ ConfigServer/src/main/java/com/dh/ConfigServer/configServerApplication.java
@@ -0,0 +1,15 @@
+package com.dh.ConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class configServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(configServerApplication.class, args);
+	}
+
+}
